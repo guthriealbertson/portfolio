9fe86fe379afc26c762e6b0685baa12c4aca90d5
guthriealbertson <tui50484@temple.edu>
2023-04-06 16:09:55 -0400

    Added color chagning background

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,53 @@
-import styles from '@/styles/Layout.module.css'
-import Navbar from './navbar';
+"use client";
+import styles from "@/styles/Layout.module.css";
+import Navbar from "./navbar";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Layout({ children }) {
+  const router = useRouter();
+  const [color, setColor] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const handleRouteChange = (url, { shallow }) => {
+      console.log(
+        `App is changing to ${url} ${
+          shallow ? "with" : "without"
+        } shallow routing`
+      );
+      switch (url) {
+        case "/":
+          setColor("#dfe7fd");
+          break;
+        case "/contact":
+          setColor("#E2ECE9");
+          break;
+        case "/about":
+          setColor("#FDE2E4");
+          break;
+        default:
+          setColor("#dfe7fd");
+      }
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, [router]);
+
   return (
-    <>
-      <Navbar/>
-      <main>{children}</main>
-    </>
+    <div>
+      <div className={styles.background} style={{ backgroundColor: color }}>
+        <Navbar />
+        <main>{children}</main>
+      </div>
+    </div>
   );
 }
---
+++ public/resume.md
@@ -0,0 +1,56 @@
+# Guthrie Albertson
+
+### Guthrie.Albertson@gmail.com | (717) 484-5056 | Philadelphia, PA
+
+**EDUCATION**
+
+**Temple University Anticipated May, 2023**
+
+_BS, Computer Science Philadelphia, PA_
+
+- Relevant Coursework:
+
+  - Cloud Computing
+
+  - Microservice Architecture
+
+  - Operating Systems
+
+  - Web Application Development
+
+  - Database Management Systems
+
+  - Data Structures and Algorithms
+
+**EXPERIENCE, SKILLS & INTERESTS**
+
+- **Experience:** Cloud hosting, System architecture, Web Development,
+  Networking, UI/UX Design
+
+- **Skills:** JavaScript, React, CSS, Google Cloud Platform, AWS EC2 &
+  S3, Django, SQL, AWS Lambda/GCP Cloud Functions, C/C++, Java, Python
+
+- **Interests:** Graphic design, Music, Urban planning,
+  Transportation, Travel, Sustainability, History
+
+**WORK EXPERIENCE**
+
+**New Holland YMCA Mar. 2018 -- Sep. 2019**
+
+_Welcome desk associate New Holland, PA_
+
+- Greeted guests and responded to guests' inquiries and issues in a
+  timely, helpful, and personal manner to resolve any guest concern.
+
+- Used and managed a large suite of software related to membership,
+  events, communication, and security.
+
+**Garden Spot Communities Jul. 2016 -- Dec. 2017**
+
+_Dietary Aide New Holland, PA_
+
+- Primarily worked in the memory care facility serving meals to
+  patients with Alzheimer's and dementia.
+
+- Worked with large teams of nurses and kitchen staff to ensure
+  residents were properly cared for.
