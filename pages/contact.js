96d6b9c6c8e2782502a48d3e5e3297b91a40a1bb
guthriealbertson <tui50484@temple.edu>
2023-04-07 02:26:33 -0400

    little improvements

--- pages/contact.js
+++ pages/contact.js
@@ -2,13 +2,18 @@ import styles from "@/styles/Contact.module.css";
 
 export default function Contact() {
   return (
-    <div className={styles.form}>
-      <div className={styles.nameemail}>
-        <input placeholder="Name" className={styles.name} />
-        <input placeholder="Email" className={styles.email} />
+    <div className={styles.page}>
+      <h1>
+        If you're looking to hire a junior web developer, send me a message!
+      </h1>
+      <div className={styles.form}>
+        <div className={styles.nameemail}>
+          <input placeholder="Name" className={styles.name} />
+          <input placeholder="Email" className={styles.email} />
+        </div>
+        <input className={styles.subject} placeholder="Subject" />
+        <textarea className={styles.message} placeholder="Message" />
       </div>
-      <input className={styles.subject} placeholder="Subject" />
-      <textarea className={styles.message} placeholder="Message" />
     </div>
   );
 }
